1085719f6d4ee12cf20291a8c4ac9fe2686d8f07
Torak28 <zelechowski28@gmail.com>
2019-04-20 12:03:27 +0200

    Add google maps and make a Contact form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,17 +5,61 @@
                 <h1>{{json.name}}</h1>
             </b-col>
         </b-row>
-        <b-row class='text'>
-            <b-col align-self="start">
+
+        <br>
+
+        <b-row>
+            <b-col>
+                <b-form>
+                    <b-input id="Imie" placeholder="Imię"></b-input>
+                </b-form>
             </b-col>
-            <b-col align-self="center">
-                {{json.text1}}
-                <br>
-                {{json.text2}}
-                <br>
-                {{json.text3}}
+            <b-col>
+                <b-form>
+                    <b-input id="Nazwisko" placeholder="Nazwisko"></b-input>
+                </b-form>
             </b-col>
-            <b-col align-self="end">
+        </b-row>
+
+        <br>
+
+        <b-row>
+            <b-col>
+                <b-form>
+                    <b-input id="Mail" placeholder="Mail"></b-input>
+                </b-form>
+            </b-col>
+        </b-row>
+
+        <br>
+
+        <b-row>
+            <b-col>
+                <b-form>
+                    <b-form-textarea id="Wiadomosc" placeholder="Napisz tutaj swoją wiadomość" rows="3" no-resize></b-form-textarea>
+                </b-form>
+            </b-col>
+        </b-row>
+
+        <br>
+
+        <b-row class='text-center'>
+            <b-col>
+                <b-button block variant="primary">Wyślij</b-button>
+            </b-col>
+        </b-row>
+
+        <br>
+        <br>
+        <br>
+
+        <b-row class='text-center'>
+            <b-col>
+                <h5>{{json.text1}}
+                <br>
+                <font-awesome-icon icon="envelope" /> {{json.text3}}
+                <br>
+                <font-awesome-icon icon="phone" /> {{json.text2}}</h5>
             </b-col>
         </b-row>
     </b-container>
@@ -41,6 +85,12 @@ export default {
     img {
         max-width: 100%;
         height: auto;
+    }
+    .test{
+        background: #ff0000 !important; 
+    }
+    .test2{
+        background: #002fff !important; 
     }
 </style>
 
